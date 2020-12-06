c2d2b527d7cd78f00dc22fdf3a762ddb4310bd2d
BurningBeans <syy102677@gmail.com>
2020-12-06 23:20:19 +0800

    Update s1093508_hw5.cpp

--- S1093508_HW5/s1093508_hw5.cpp
+++ S1093508_HW5/s1093508_hw5.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 using std::cout;
+template <typename T>
+void mySwap(T &a, T &b)
+{
+    auto t = a;
+    a = b;
+    b = t;
+}
+/* function overloading
 void mySwap(double &a, double &b)
 {
     double t = a;
     a = b;
     b = t;
 }
-void mySwap(int &a, int &b)//function overloading
+void mySwap(int &a, int &b)
 {
     int t = a;
     a = b;
     b = t;
 }
+*/
 int main()
 {
  int a = 3;
